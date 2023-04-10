e2b611aa9409e4ad89bdaf0053d65a27af5cde71
Mohit030802 <mohitom2002@gmail.com>
2023-04-10 12:33:39 +0530

    45

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ChatBubbleOutlined,
+    FavoriteBorderOulines,
+    FavoriteOutlined,
+    ShareOutlined
+} from '@mui/icons-material'
+import {Box, Divider,Iconbutton,Typography,useTheme } from '@mui/material';
+import FlexBetween from '../../components/FlexBetween';
+import Friend from "components/Friend"
+
+const PostWidget = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostWidget
